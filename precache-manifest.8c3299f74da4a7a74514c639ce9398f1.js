dd386635c7b66243d143907f8017abb2d1416fc0
hakeemullah <h.jan210@gmail.com>
2019-04-19 16:25:34 +0500

    Updates

---
+++ precache-manifest.8c3299f74da4a7a74514c639ce9398f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bulb-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ae0c00bbf0dd71a52fa",
+    "url": "/bulb-app/static/js/runtime~main.9342f428.js"
+  },
+  {
+    "revision": "ad1436ac618c8b3d83f7",
+    "url": "/bulb-app/static/js/main.656b9afb.chunk.js"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/bulb-app/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "ad1436ac618c8b3d83f7",
+    "url": "/bulb-app/static/css/main.3099e4a2.chunk.css"
+  },
+  {
+    "revision": "86f27e006e1ea562851884ed4b116704",
+    "url": "/bulb-app/index.html"
+  }
+];
