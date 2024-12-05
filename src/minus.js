fa845d4bcbd18c6154c31be0a3ea25befe60a6e5
akira <akira@drox.com>
2024-12-05 11:19:55 +0000

    add minus.js file for subtraction function

---
+++ src/minus.js
@@ -0,0 +1,13 @@
+// src/minus.js
+
+/**
+ * Subtracts two numbers and returns the result.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The result of subtracting b from a.
+ */
+function subtract(a, b) {
+    return a - b;
+}
+
+module.exports = subtract;
